a9291088eabd2bae4bdc5f19a868c5f75c5a4879
Nguyen Luan <luan.nt@tinasoft.vn>
2022-09-16 21:16:03 +0700

    feat(forgot-password) - man đăng nhap

--- api/ApiUser.tsx
+++ api/ApiUser.tsx
@@ -104,8 +104,20 @@ export interface IParamsGetUser {
   };
 }
 
+  export interface IForgotPassword{
+    email:string;
+  }
+
+  export interface ISetPassword{
+    email: string;
+    otp: string;
+    newPassword: string;
+  }
+
 const path = {
   login: "/auth/login",
+  forgotpassword:"/auth/forgot-password",
+  setpassword:"/auth/set-password",
   getMe: "/users/me",
   getUserAccount: "/users",
   uploadAvatar: "/users/set-avatar",
@@ -187,6 +199,17 @@ function login(body: ILoginBody): Promise<ILoginResponse> {
   );
 }
 
+function forgotPassword(body: IForgotPassword) : Promise<IUserLogin> {
+  return fetcher(
+    {url: path.forgotpassword, method: "post", data: body},
+  )
+}
+
+function setPassword(body: ISetPassword) : Promise<IUserLogin> {
+  return fetcher(
+    {url: path.setpassword, method: "post", data: body},
+  )
+}
 function addNewEmployee(body: IRegisterAccountBody): Promise<IUserLogin> {
   return fetcher({url: path.addNewEmployee, method: "post", data: body});
 }
@@ -260,6 +283,8 @@ function getAuthToken(): string | undefined {
 
 export default {
   login,
+  forgotPassword,
+  setPassword,
   isLogin,
   getAuthToken,
   getInfoMe,
--- module/login/index.tsx
+++ module/login/index.tsx
@@ -1,16 +1,13 @@
 import "./index.scss";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {NewPassword} from "@app/module/login/NewPassword";
 import {ForgotPassword} from "@app/module/login/ForgotPassword";
-import {VerifyPassword} from "@app/module/login/VerifyPassword";
+import {InputOTP} from "@app/module/login/InputOTP";
 import {SignIn} from "@app/module/login/SignIn";
 
 export function Login(): JSX.Element {
-  // const [data, setData] = useState({
-  //   email: "",
-  //   code: "",
-  //   password: "",
-  // });
+  const [data, setData] = useState("")
+
 
   const [tab, setTab] = useState("signIn");
 
@@ -20,14 +17,16 @@ export function Login(): JSX.Element {
     },
     forgotPassword: {
       component: ForgotPassword,
+      
     },
-    verifyPassword: {
-      component: VerifyPassword,
+    inputOTP: {
+      component: InputOTP,
     },
     newPassword: {
       component: NewPassword,
     },
   };
+
 
   return (
     <div className="container-login">
@@ -35,9 +34,10 @@ export function Login(): JSX.Element {
         <div className="form">
           {React.createElement(tabList[tab as keyof typeof tabList].component, {
             changeTab: setTab,
-            // data: data,
-            // setData: setData,
-          })}
+            setData:setData,
+            data:data,
+            
+             })}
         </div>
       </div>
     </div>
